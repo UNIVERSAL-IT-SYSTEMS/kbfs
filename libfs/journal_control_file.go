cd5aa3e366f344e5b39c39fc3a50976a08c5d8fd
Frederick Akalin <akalin@keyba.se>
2016-10-04 01:12:11 -0700

    Fix rest

--- libfs/journal_control_file.go
+++ libfs/journal_control_file.go
@@ -10,6 +10,7 @@ import (
 	"golang.org/x/net/context"
 
 	"github.com/keybase/kbfs/libkbfs"
+	"github.com/keybase/kbfs/tlf"
 )
 
 // JournalAction enumerates all the possible actions to take on a
@@ -51,33 +52,33 @@ func (a JournalAction) String() string {
 // given TLF.
 func (a JournalAction) Execute(
 	ctx context.Context, jServer *libkbfs.JournalServer,
-	tlf libkbfs.TlfID) error {
-	if tlf == (libkbfs.TlfID{}) {
+	tlfID tlf.TlfID) error {
+	if tlfID == (tlf.TlfID{}) {
 		panic("zero TlfID in JournalAction.Execute")
 	}
 
 	switch a {
 	case JournalEnable:
 		err := jServer.Enable(
-			ctx, tlf, libkbfs.TLFJournalBackgroundWorkEnabled)
+			ctx, tlfID, libkbfs.TLFJournalBackgroundWorkEnabled)
 		if err != nil {
 			return err
 		}
 
 	case JournalFlush:
-		err := jServer.Flush(ctx, tlf)
+		err := jServer.Flush(ctx, tlfID)
 		if err != nil {
 			return err
 		}
 
 	case JournalPauseBackgroundWork:
-		jServer.PauseBackgroundWork(ctx, tlf)
+		jServer.PauseBackgroundWork(ctx, tlfID)
 
 	case JournalResumeBackgroundWork:
-		jServer.ResumeBackgroundWork(ctx, tlf)
+		jServer.ResumeBackgroundWork(ctx, tlfID)
 
 	case JournalDisable:
-		_, err := jServer.Disable(ctx, tlf)
+		_, err := jServer.Disable(ctx, tlfID)
 		if err != nil {
 			return err
 		}
